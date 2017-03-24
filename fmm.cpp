184de0953421759f89cd8f78630625278ba53f9b
Vashirawit <vakkharasaksiri@hotmail.com>
2017-03-24 23:47:50 +0700

    test push

--- fmm.cpp
+++ fmm.cpp
@@ -9,5 +9,6 @@ int main(){
 	for(int i=1;i<=n;i++)cin >> a[i];
 	dp[0]=dp[1]=dp[2]=0;
 	for(int i=3;i<=n;i++)dp[i] = max((i!=n) * (dp[i-3]+a[i]),max(dp[i-1],dp[i-2]));
-	
+	cout << dp[n];
+	cout << "ggggfffffmmmmm";
 }
