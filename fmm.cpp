f1a1c3ab99438fe17b744f4d0f9c6dc20df4a39a
Ekkalak Leelasornchai <ekkalakfm@gmail.com>
2017-03-24 14:58:43 +0700

    test

---
+++ fmm.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[200000],a[200000];
+int main(){
+	int n;
+	cin >> n;
+	for(int i=1;i<=n;i++)cin >> a[i];
+	dp[0]=dp[1]=dp[2]=0;
+	for(int i=3;i<=n;i++)dp[i] = max((i!=n) * (dp[i-3]+a[i]),max(dp[i-1],dp[i-2]));
+	cout << dp[n];
+}
